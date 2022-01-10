8e421b924645ebb3f750e37f6e972d8bd9ecb4c0
ravilella1234 <masters.selenium@gmail.com>
2022-01-10 09:03:51 +0530

    TestNG DAy-1

---
+++ MavenProject/src/test/java/pack/NewTest.java
@@ -0,0 +1,11 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+  @Test
+  public void f() 
+  {
+  }
+}
---
+++ MavenProject/src/test/java/pack/NewTest1.java
@@ -0,0 +1,12 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 
+{
+  @Test
+  public void f() 
+  {
+	  System.out.println("iam f test");
+  }
+}
